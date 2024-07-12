aedf197a7bf2ba54f1b58fd6772b85ce8bfa596b
zkvlkat <132288380+zkvlkat@users.noreply.github.com>
2024-07-13 07:11:22 +0900

    Update app.py

--- CatholicUniversity_Scraping/deploy/app.py
+++ CatholicUniversity_Scraping/deploy/app.py
@@ -14,16 +14,17 @@ def update_info():
                 ("기타",["해커톤", "e스포츠", "e-스포츠", "아카데미", "Academy", "ICT", "클라우드", "cloud",     "창업","분석", "아이디어", "컨퍼런스", "IOT"])
         ]
         data = {key:[] for key, _ in keywords_list}
-        
-        for i in range(1,11): #30days data
-            page_number = f"페이지{i}"
-            url = f'https://www.catholic.ac.kr/front/boardlist.do?bbsConfigFK=24&cmsDirPkid=2055&cmsLocalPkid=1&searchField=ALL&searchValue=&currentPage={i}&searchLowItem=ALL'
+        num = 1
+        for i in range(0, 91, 10):
+            page_number = f"페이지{num}"
+            url = f'https://www.catholic.ac.kr/ko/campuslife/notice_outside.do?mode=list&&articleLimit=10&article.offset={i}'
             for key, keywords in keywords_list:
                 temp_info = scraping.get_web_info(url,keywords)
             if not temp_info:
                 temp_info = ['내용 없음']
             temp_info.insert(0,page_number)
             data[key].append(temp_info)
+            num += 1
         return render_template("update.html",data=data)
 
 if __name__ == '__main__':
